d5729680d881c3eee72502e79a7ea4b1d809f641
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-05-02 03:11:37 +0000

    change js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Manya All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Manya
+// Created on: May 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+function calculate() {
+  const age = parseInt(document.getElementById("age").value)
+  const day = parseInt(document.getElementById("day").value)
+
+  if ( (day == "Tuesday" || day == "Thursday") || age > 12 && age < 21 ) {
+    document.getElementById("answer").innerHTML =
+      "You're eligible for student pricing!"
+  } else {
+    document.getElementById("answer").innerHTML =
+      "You must pay regular price."
+  }
+  console.log("Thanks for verifying your age!");
+}
