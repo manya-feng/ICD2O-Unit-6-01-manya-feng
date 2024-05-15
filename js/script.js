edd838c7471f86c0c8d28c2c296191fc58351230
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-05-15 18:30:13 +0000

    change

--- js/script.js
+++ js/script.js
@@ -3,7 +3,21 @@
 // Created by: Manya
 // Created on: May 2024
 // This file contains the JS functions for index.html
+
 "use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+    scope: "/ICS2O-PWA-Test/",
+  })
+}
+
+/**
+ * This function displays an alert.
+ */
 
 function calculate() {
   const age = parseInt(document.getElementById("age").value)
